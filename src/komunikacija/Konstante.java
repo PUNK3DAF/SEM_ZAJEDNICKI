df7fcc24f13f0482a111fee855eef25a8e47bb8c
Vld Mrk <vm20190346@student.fon.bg.ac.rs>
2025-12-22 23:37:01 +0100

    Removed license comments from multiple domain and communication classes; added Konstante class for ensemble and member messages.

---
+++ src/komunikacija/Konstante.java
@@ -0,0 +1,43 @@
+package komunikacija;
+
+public class Konstante {
+    
+    // Ensemble Messages
+    public static final String ENSEMBLE_LOADED = "Sistem je ucitao ansambl.";
+    public static final String ENSEMBLE_SAVED = "Sistem je zapamtio ansambl.";
+    public static final String ENSEMBLE_CREATED = "Sistem je kreirao ansambl.";
+    public static final String ENSEMBLE_DELETED = "Sistem je obrisao ansambl.";
+    public static final String ENSEMBLE_NOT_SELECTED = "Sistem ne moze da ucita ansambl.";
+    
+    // Member Messages
+    public static final String MEMBER_CREATED = "Sistem je kreirao clana drustva.";
+    public static final String MEMBER_SAVED = "Sistem je zapamtio clana drustva.";
+    public static final String MEMBER_NOT_SELECTED = "Sistem ne moze da ucita clana.";
+    public static final String MEMBER_NOT_AVAILABLE = "Originalni clan nije dostupan za izmenu.";
+    
+    // Search Messages
+    public static final String SEARCH_FOUND = "Sistem je nasao ansamble po zadatoj vrednosti.";
+    public static final String SEARCH_NOT_FOUND = "Sistem ne moze da nadje ansamble po zadatoj vrednosti.";
+    
+    // Error Messages
+    public static final String ERROR_CREATE_ENSEMBLE = "Sistem ne moze da kreira ansambl.";
+    public static final String ERROR_SAVE_ENSEMBLE = "Sistem ne moze da zapamti ansambl.";
+    public static final String ERROR_DELETE_ENSEMBLE = "Sistem ne moze da obrise ansambl.";
+    public static final String ERROR_CREATE_MEMBER = "Sistem ne moze da kreira clana drustva.";
+    public static final String ERROR_SAVE_MEMBER = "Sistem ne moze da zapamti clana drustva.";
+    public static final String ERROR_LOAD_ENSEMBLE = "Greska prilikom ucitavanja ansambla.";
+    public static final String ERROR_YEARS_INVALID = "Godine moraju biti broj.";
+    public static final String ERROR_REQUIRED_FIELDS = "Ime i opis su obavezni.";
+    public static final String ERROR_REQUIRED_NAME = "Ime ansambla je obavezno.";
+    public static final String ERROR_REQUIRED_DESCRIPTION = "Opis ansambla je obavezan.";
+    
+    // Dialog Titles
+    public static final String TITLE_SUCCESS = "USPEH";
+    public static final String TITLE_ERROR = "GRESKA";
+    public static final String TITLE_INFO = "Informacija";
+    public static final String TITLE_CONFIRM = "Potvrda brisanja";
+    public static final String TITLE_ENSEMBLE_DETAILS = "Detalji ansambla";
+    
+    // Confirmation Messages
+    public static final String CONFIRM_DELETE = "Da li ste sigurni da zelite da obrisete ansambl?";
+}
