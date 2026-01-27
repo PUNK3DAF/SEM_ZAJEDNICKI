b5250771c3584d7519b500c44ac44e3ca30fc286
Vld Mrk <vm20190346@student.fon.bg.ac.rs>
2026-01-27 18:09:25 +0100

    Refactored Dogadjaj class to remove Serializable implementation, updated constructor and methods to manage mestoID, and enhanced database operations for better data handling.

--- src/domen/Dogadjaj.java
+++ src/domen/Dogadjaj.java
@@ -4,35 +4,38 @@
  */
 package domen;
 
-import java.io.Serializable;
+import java.sql.ResultSet;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author vldmrk
  */
-public class Dogadjaj extends ApstraktniDomenskiObjekat implements Serializable {
+public class Dogadjaj implements ApstraktniDomenskiObjekat {
 
-    private Integer dogadjajID;
+    private int dogadjajID;
     private String naziv;
     private LocalDate datum;
+    private int mestoID;
     private Mesto mesto;
 
     public Dogadjaj() {
     }
 
-    public Dogadjaj(Integer dogadjajID, String naziv, LocalDate datum, Mesto mesto) {
+    public Dogadjaj(int dogadjajID, String naziv, LocalDate datum, int mestoID) {
         this.dogadjajID = dogadjajID;
         this.naziv = naziv;
         this.datum = datum;
-        this.mesto = mesto;
+        this.mestoID = mestoID;
     }
 
-    public Integer getDogadjajID() {
+    public int getDogadjajID() {
         return dogadjajID;
     }
 
-    public void setDogadjajID(Integer dogadjajID) {
+    public void setDogadjajID(int dogadjajID) {
         this.dogadjajID = dogadjajID;
     }
 
@@ -52,32 +55,107 @@ public class Dogadjaj extends ApstraktniDomenskiObjekat implements Serializable 
         this.datum = datum;
     }
 
+    public int getMestoID() {
+        return mestoID;
+    }
+
+    public void setMestoID(int mestoID) {
+        this.mestoID = mestoID;
+    }
+
     public Mesto getMesto() {
         return mesto;
     }
 
     public void setMesto(Mesto mesto) {
         this.mesto = mesto;
+        if (mesto != null) {
+            this.mestoID = mesto.getMestoID();
+        }
+    }
+
+    @Override
+    public String vratiNazivTabele() {
+        return "dogadjaj";
+    }
+
+    @Override
+    public List<ApstraktniDomenskiObjekat> vratiListu(ResultSet rs) throws Exception {
+        List<ApstraktniDomenskiObjekat> lista = new ArrayList<>();
+        while (rs.next()) {
+            Dogadjaj d = new Dogadjaj();
+            d.setDogadjajID(rs.getInt("dogadjajID"));
+            d.setNaziv(rs.getString("naziv"));
+            d.setDatum(rs.getDate("datum").toLocalDate());
+            d.setMestoID(rs.getInt("mestoID"));
+            
+            // If mesto name is available from joined result, create Mesto object
+            try {
+                String mestoNaziv = rs.getString("mestoNaziv");
+                if (mestoNaziv != null) {
+                    Mesto m = new Mesto();
+                    m.setMestoID(d.getMestoID());
+                    m.setNaziv(mestoNaziv);
+                    d.setMesto(m);
+                }
+            } catch (Exception e) {
+                // Column doesn't exist, skip
+            }
+            lista.add(d);
+        }
+        return lista;
+    }
+
+    @Override
+    public String vratiKoloneZaUbacivanje() {
+        return "naziv, datum, mestoID";
+    }return n + ", " + d + ", " + mestoID
+    @Override
+    public String vratiVrednostiZaUbacivanje() {
+        String n = (naziv == null) ? "NULL" : "'" + naziv.replace("'", "''") + "'";
+        String d = (datum == null) ? "NULL" : "'" + datum.toString() + "'";
+        String m = (mesto == null || mesto.getMestoID() == null) ? "NULL" : mesto.getMestoID().toString();
+        return n + ", " + d + ", " + m;
+    }
+
+    @Override
+    public String vratiPrimarniKljuc() {
+        return "dogadjajID=" + dogadjajID;
+    }
+
+    @Override
+    public ApstraktniDomenskiObjekat vratiObjekatIzRS(ResultSet rs) throws Exception {
+        Dogadjaj d = new Dogadjaj();
+        d.setDogadjajID(rs.getInt("dogadjajID"));
+        d.setNaziv(rs.getString("naziv"));
+        d.setDatum(rs.getDate("datum").toLocalDate());
+        
+        d.setMestoID(rs.getInt("mestoID"));
+        
+        // If mesto name is available from joined result, create Mesto object
+        try {
+            String mestoNaziv = rs.getString("mestoNaziv");
+            if (mestoNaziv != null) {
+                Mesto m = new Mesto();
+                m.setMestoID(d.getMestoID());
+                m.setNaziv(mestoNaziv);
+                d.setMesto(m);
+            }
+        } catch (Exception e) {
+            // Column doesn't exist, skip
+        return d;
+    }
+return "naziv=" + n + ", datum=" + d + ", mestoID=" + mestoID
+    public String vratiVrednostiZaIzmenu() {
+        String n = (naziv == null) ? "NULL" : "'" + naziv.replace("'", "''") + "'";
+        String d = (datum == null) ? "NULL" : "'" + datum.toString() + "'";
+        String m = (mesto == null || mesto.getMestoID() == null) ? "NULL" : mesto.getMestoID().toString();
+        return "naziv=" + n + ", datum=" + d + ", mestoID=" + m;
     }
 
     @Override
     public String toString() {
         return naziv;
     }
-
-    @Override
-    public String getNazivTabele() {
-        return "dogadjaj";
-    }
-
-    @Override
-    public String getNazivKljuca() {
-        return "dogadjajID";
-    }
-
-    @Override
-    public String getVrednostKljuca() {
-        return dogadjajID.toString();
-    }
-
 }
+
