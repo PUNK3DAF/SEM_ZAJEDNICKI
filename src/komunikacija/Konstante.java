12dfaf92b3dc0dd4b550e453daba4f31acadfb6b
Vld Mrk <vm20190346@student.fon.bg.ac.rs>
2026-07-10 14:05:15 +0200

    Corrected spelling and accentuation in string constants for improved readability and consistency.

--- src/komunikacija/Konstante.java
+++ src/komunikacija/Konstante.java
@@ -2,36 +2,36 @@ package komunikacija;
 
 public class Konstante {
     
-    public static final String ENSEMBLE_LOADED = "Sistem je ucitao ansambl.";
+    public static final String ENSEMBLE_LOADED = "Sistem je učitao ansambl.";
     public static final String ENSEMBLE_SAVED = "Sistem je zapamtio ansambl.";
     public static final String ENSEMBLE_CREATED = "Sistem je kreirao ansambl.";
     public static final String ENSEMBLE_DELETED = "Sistem je obrisao ansambl.";
-    public static final String ENSEMBLE_NOT_SELECTED = "Sistem ne moze da ucita ansambl.";
+    public static final String ENSEMBLE_NOT_SELECTED = "Sistem ne može da učita ansambl.";
     
-    public static final String MEMBER_CREATED = "Sistem je kreirao clana drustva.";
-    public static final String MEMBER_SAVED = "Sistem je zapamtio clana drustva.";
-    public static final String MEMBER_NOT_SELECTED = "Sistem ne moze da ucita clana.";
-    public static final String MEMBER_NOT_AVAILABLE = "Originalni clan nije dostupan za izmenu.";
+    public static final String MEMBER_CREATED = "Sistem je kreirao člana društva.";
+    public static final String MEMBER_SAVED = "Sistem je zapamtio člana društva.";
+    public static final String MEMBER_NOT_SELECTED = "Sistem ne može da učita člana.";
+    public static final String MEMBER_NOT_AVAILABLE = "Originalni član nije dostupan za izmenu.";
     
-    public static final String SEARCH_FOUND = "Sistem je nasao ansamble po zadatoj vrednosti.";
-    public static final String SEARCH_NOT_FOUND = "Sistem ne moze da nadje ansamble po zadatoj vrednosti.";
+    public static final String SEARCH_FOUND = "Sistem je našao ansamble po zadatoj vrednosti.";
+    public static final String SEARCH_NOT_FOUND = "Sistem ne može da nađe ansamble po zadatoj vrednosti.";
     
-    public static final String ERROR_CREATE_ENSEMBLE = "Sistem ne moze da kreira ansambl.";
-    public static final String ERROR_SAVE_ENSEMBLE = "Sistem ne moze da zapamti ansambl.";
-    public static final String ERROR_DELETE_ENSEMBLE = "Sistem ne moze da obrise ansambl.";
-    public static final String ERROR_CREATE_MEMBER = "Sistem ne moze da kreira clana drustva.";
-    public static final String ERROR_SAVE_MEMBER = "Sistem ne moze da zapamti clana drustva.";
-    public static final String ERROR_LOAD_ENSEMBLE = "Greska prilikom ucitavanja ansambla.";
+    public static final String ERROR_CREATE_ENSEMBLE = "Sistem ne može da kreira ansambl.";
+    public static final String ERROR_SAVE_ENSEMBLE = "Sistem ne može da zapamti ansambl.";
+    public static final String ERROR_DELETE_ENSEMBLE = "Sistem ne može da obriše ansambl.";
+    public static final String ERROR_CREATE_MEMBER = "Sistem ne može da kreira člana društva.";
+    public static final String ERROR_SAVE_MEMBER = "Sistem ne može da zapamti člana društva.";
+    public static final String ERROR_LOAD_ENSEMBLE = "Greška prilikom učitavanja ansambla.";
     public static final String ERROR_YEARS_INVALID = "Godine moraju biti broj.";
     public static final String ERROR_REQUIRED_FIELDS = "Ime i opis su obavezni.";
     public static final String ERROR_REQUIRED_NAME = "Ime ansambla je obavezno.";
     public static final String ERROR_REQUIRED_DESCRIPTION = "Opis ansambla je obavezan.";
     
     public static final String TITLE_SUCCESS = "USPEH";
-    public static final String TITLE_ERROR = "GRESKA";
+    public static final String TITLE_ERROR = "GREŠKA";
     public static final String TITLE_INFO = "Informacija";
     public static final String TITLE_CONFIRM = "Potvrda brisanja";
     public static final String TITLE_ENSEMBLE_DETAILS = "Detalji ansambla";
     
-    public static final String CONFIRM_DELETE = "Da li ste sigurni da zelite da obrisete ansambl?";
+    public static final String CONFIRM_DELETE = "Da li ste sigurni da želite da obrišete ansambl?";
 }
